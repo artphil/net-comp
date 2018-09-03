53c4b05c738dae750a80bee562547bdd1b3e3701
artphil <artphil.dev@gmail.com>
2018-09-03 19:17:22 -0300

    servidor guarda cliente e compara hash

--- src/servidor.py
+++ src/servidor.py
@@ -4,7 +4,20 @@
 
 import socket
 import sys
-from struct import unpack, calcsize
+import crypt
+from struct import unpack
+from time import time
+from hmac import compare_digest as compare_hash
+
+def dprint (d):
+	print('--------------Data----------------')
+	for k, v in d.items():
+		print (k)
+		for kk, vv in v.items():
+			print ('	', kk, '	:', vv)
+		print()
+	print('----------------------------------')
+
 
 HOST = ''					# Endereco IP do Servidor
 PORT = int(sys.argv[2])		# Porta que o Servidor esta
@@ -20,10 +33,35 @@ udp.bind(orig)
 
 # Tratamento de conversas
 variaveis = ['id', 'seg', 'ns', 'sz', 'msg', 'md5']
+v_pack = ['L', 'L', 'I', 'H']
 cliente_list = {}
 
 while True:
 	msg, cliente = udp.recvfrom(1024)
-	print (cliente, msg)
+	if cliente not in cliente_list:
+		cliente_list[cliente] = {}
+		ordem = cliente_list[cliente]['ordem'] = 0
+		cliente_list[cliente]['tempo'] = time()
+		cliente_list[cliente][variaveis[ordem]] = unpack('L', msg)[0]
+	else:
+		cliente_list[cliente]['tempo'] = time()
+		ordem = cliente_list[cliente]['ordem']
+		if ordem < 4:
+			cliente_list[cliente][variaveis[ordem]] = unpack(v_pack[ordem], msg)[0]
+		else:
+			cliente_list[cliente][variaveis[ordem]] = msg.decode('latin1')
+
+	cliente_list[cliente]['ordem'] += 1
+
+	if cliente_list[cliente]['ordem'] > 5:
+		cliente_list[cliente]['ordem'] = 0
+		data = ''
+		for value in variaveis[:-1]:
+			data += str(cliente_list[cliente][value])
+		chash = compare_hash(crypt.crypt(data, cliente_list[cliente]['md5']), cliente_list[cliente]['md5'])
+
+		print ('chahs= ', chash)
+
+	dprint(cliente_list)
 
 udp.close()
