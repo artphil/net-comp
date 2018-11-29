7b760fb4739e59c90893d127157020b6acb5f81d
Gabriel7x <gabriel.almeida7x7@gmail.com>
2018-11-28 22:54:57 -0200

    Server Version 0.01

--- src/server.py
+++ src/server.py
@@ -1,13 +1,39 @@
-import os
-from flask import Flask, json
+'''
+Universidade Federal de Minas Gerais
+Trabalho pratico da disciplina Rede de Computadores da UFMG
+Protocolo HTTP e servico REST
+Arthur Phillip D. Silva & Gabriel Almeida de Jesus
+Servidor
+'''
 
-app = Flask(__name__)
+import json
+import socket
+import sys
 
+MAX_TAM = 1024
 
-@app.route('/')
-def home():
-	return "Redes de Computadores DCC - UFMG"
+HOST = '' # Endereco de IP do Servidor
+PORT = int(sys.argv[1]) # Porta que o Servidor esta
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+
+# Abertura passiva de Conexão
+tcp.bind(orig)
+tcp.listen(1)
+print('Servidor Escutando')
+
+def responde(con, cliente):
+	print('Conectado com ', cliente)
+	request = con.recv(MAX_TAM).decode('latin1')
+
+	print('Requisicao = ', request)
+
+while True:
+	# Aceitando Conexões
+	con, cliente = tcp.accept()
+	con.settimeout(15)
+	responde(con, cliente)
+
+# Fecha Conexão
+tcp.close()
